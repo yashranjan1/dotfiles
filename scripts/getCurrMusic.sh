bf2eecd8460f2d37e72744e881a6a5656035ed8f
Yash <yash.ranjan25@gmail.com>
2024-09-20 02:04:38 +0530

    Changes: - hypridle setup - minor hyprlock changes

--- scripts/getCurrMusic.sh
+++ scripts/getCurrMusic.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Get the currently playing song and artist from MPRIS players (VLC, Rhythmbox, etc.)
-current_song=$(playerctl metadata title)
-current_artist=$(playerctl metadata artist)
+current_song=$(playerctl metadata title 2>/dev/null)
+current_artist=$(playerctl metadata artist 2>/dev/null)
 
-# Display the song and artist
-echo "Currently Playing: $current_song - $current_artist"
+# Check if both song and artist are not empty
+if [[ -n "$current_song" && -n "$current_artist" ]]; then
+    # Display the song and artist
+    echo "$current_song - $current_artist"
+fi
